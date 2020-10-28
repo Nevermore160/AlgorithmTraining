ec4fae4cac5ec10dbe2ac94bcd8b32a7dc38fb25
Nevermore <1215632564@qq.com>
2020-10-28 09:50:00 +0800

    算法训练更新

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -8,6 +8,19 @@ public class HelloWord {
 
     @SuppressWarnings("all") //压制警告
     static class Solution {
+        public boolean uniqueOccurrences(int[] arr) {
+            Map<Integer,Integer> map = new HashMap<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (map.containsKey(arr[i])) map.put(arr[i],map.get(arr[i]) + 1);
+                else map.put(arr[i], 1);
+            }
+            Set<Integer> set = new HashSet<>();
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                set.add(entry.getValue());
+            }
+            return set.size() == map.size();
+        }
+
         public static List<Integer> preorderTraversal(TreeNode root) {
             ArrayDeque<TreeNode> deque = new ArrayDeque<>();
             List<Integer> list = new LinkedList<>();
